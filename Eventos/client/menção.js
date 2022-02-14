b4667fb37147289ffcf06199250d490a5293fb2f
Aubrey <96559615+AubreyFBG@users.noreply.github.com>
2022-02-14 20:58:27 -0300

    Create menção.js

---
+++ Eventos/client/menção.js
@@ -0,0 +1,19 @@
+const client = require("../../index");
+const Discord = require("discord.js")
+
+client.on("messageCreate", message => {
+ 
+  if(message.author.bot) return;
+  if(!message.guild) return;
+  
+  if(message.content == `<@${client.user.id}>` || message.content == `<@!${client.user.id}>`){
+  
+  const embed = new  Discord.MessageEmbed()
+    .setTitle(`${client.user.username}`)
+    .setColor(`a5d7ff`)
+    .setThumbnail(client.user.avatarURL())
+    .setDescription(`> **Opa! Bão?** Me chamo \`${client.user.username}\`, se precisar de ajuda use /help`)
+
+   message.reply({embeds: [embed]})
+  }
+});
